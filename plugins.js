0ed7622121b9c60db126f56b20941ab30d524143
IsmailBourbie <bourbieismail@gmail.com>
2020-01-17 22:49:18 +0100

    add sort table

--- plugins.js
+++ plugins.js
@@ -1,18 +1,18 @@
 var sendBtn = document.getElementById("send");
 
-sendBtn.addEventListener("click", function() {
+sendBtn.addEventListener("click", function () {
     var xhttp;
 
     // Test Support Browsers
     if (window.XMLHttpRequest) {
         // code for modern browsers
         xhttp = new XMLHttpRequest();
-      } else {
+    } else {
         // code for old IE browsers
         xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-     }
-    
-     // Check the status of the request
+    }
+
+    // Check the status of the request
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             // display data when the request finished and response is ready
@@ -22,7 +22,7 @@ sendBtn.addEventListener("click", function() {
 
     // send the request to the xml file;
     xhttp.open("GET", "book_catalog.xml", true);
-    xhttp.send(); 
+    xhttp.send();
 });
 
 function displayResult(xml) {
@@ -30,7 +30,7 @@ function displayResult(xml) {
         xmlDoc = xml.responseXML,
         table = document.getElementById('demo')
         tbody = document.getElementById('tableBody'),
-        x = xmlDoc.getElementsByTagName('BOOK');
+        x = xmlDoc.getElementsByTagName('BOOK'),
         tbody.innerHTML = '';
 
     // fill the table with xml data
@@ -43,22 +43,22 @@ function displayResult(xml) {
             tdPrice = document.createElement('td'),
             tdYear = document.createElement('td');
 
-            // append returned data to created columns 
-            tdIndex.append(i+1);
-            tdAuthor.append(x[i].getElementsByTagName("AUTHOR")[0].childNodes[0].nodeValue);
-            tdTitle.append(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
-            tdPrice.append(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
-            tdYear.append(x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue);
+        // append returned data to created columns 
+        tdIndex.append(i + 1);
+        tdAuthor.append(x[i].getElementsByTagName("AUTHOR")[0].childNodes[0].nodeValue);
+        tdTitle.append(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
+        tdPrice.append(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
+        tdYear.append(x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue);
 
-            // append columns to row 
-            tr.appendChild(tdIndex);
-            tr.appendChild(tdAuthor);
-            tr.appendChild(tdTitle);
-            tr.appendChild(tdPrice);
-            tr.appendChild(tdYear);
+        // append columns to row 
+        tr.appendChild(tdIndex);
+        tr.appendChild(tdAuthor);
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdPrice);
+        tr.appendChild(tdYear);
 
-            // append row to Tbody
-            tbody.appendChild(tr);
+        // append row to Tbody
+        tbody.appendChild(tr);
     }
     // append Tbody to the demo table
     table.appendChild(tbody);
@@ -66,4 +66,65 @@ function displayResult(xml) {
     // show the table and title
     table.style.display = "table";
     document.getElementById("tableTitle").style.display = "block";
+}
+
+// on th Click sort the table and change the style
+var th = document.getElementsByTagName('th'),
+    j,
+    k = 0;
+for(let j = 0; j < th.length; j++) {
+    th[j].addEventListener("click", function () {
+        th[k].classList.remove('selected');
+        k = j;
+        sortTable(j);
+        th[j].classList.add('selected');
+    });
+
+}
+
+function sortTable(tdIndex) {
+    var tableBody, rows, switching, i, x, y, shouldSwitch;
+    tableBody = document.getElementById("tableBody");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = tableBody.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 0; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[tdIndex];
+            // console.log(x);
+            y = rows[i + 1].getElementsByTagName("TD")[tdIndex];
+
+            if (tdIndex == 1 || tdIndex == 2) {
+                //check if the two rows should switch place:
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else {
+                    //check if the two rows should switch place:
+                if (Number.parseFloat(x.innerHTML) > Number.parseFloat(y.innerHTML)) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+
+            }            
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
 }
