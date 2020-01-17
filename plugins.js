74ae2200a4304ad0f070ca01527bc3d8dc046a3e
IsmailBourbie <bourbieismail@gmail.com>
2020-01-17 15:52:55 +0100

    refactor js code, add some comments

--- plugins.js
+++ plugins.js
@@ -1,32 +1,68 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
+var sendBtn = document.getElementById("send");
+
+sendBtn.addEventListener("click", function() {
+    var xhttp;
+
+    // Test Support Browsers
+    if (window.XMLHttpRequest) {
+        // code for modern browsers
+        xhttp = new XMLHttpRequest();
+      } else {
+        // code for old IE browsers
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+     }
+    
+     // Check the status of the request
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            // display data when the request finished and response is ready
+            displayResult(this);
         }
     };
-    xhttp.open("GET", "book_catalog.xml", true);
-    xhttp.send();
-}
-function myFunction(xml) {
-    var i;
-    var xmlDoc = xml.responseXML;
 
-    console.log(xml);
-    var content = "";
-    var x = xmlDoc.getElementsByTagName("BOOK");
+    // send the request to the xml file;
+    xhttp.open("GET", "book_catalog.xml", true);
+    xhttp.send(); 
+});
+
+function displayResult(xml) {
+    var i,
+        xmlDoc = xml.responseXML,
+        table = document.getElementById('demo')
+        tbody = document.getElementById('tableBody'),
+        x = xmlDoc.getElementsByTagName('BOOK');
+        tbody.innerHTML = '';
+
+    // fill the table with xml data
     for (i = 0; i < x.length; i++) {
-        content += "<tr><td>" +
-            (i+1) + "</td><td>" +
-            x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-            "</td><td>" +
-            x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-            "</td><td>" +
-            x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue +
-            "</td><td>" +
-            x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue +
-            "</td></tr>";
+        // Create Row of table and Column
+        var tr = document.createElement('tr'),
+            tdIndex = document.createElement('td'),
+            tdArtist = document.createElement('td'),
+            tdTitle = document.createElement('td'),
+            tdPrice = document.createElement('td'),
+            tdYear = document.createElement('td');
+
+            // append returned data to created columns 
+            tdIndex.append(i+1);
+            tdArtist.append(x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue);
+            tdTitle.append(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue);
+            tdPrice.append(x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue);
+            tdYear.append(x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue);
+
+            // append columns to row 
+            tr.appendChild(tdIndex);
+            tr.appendChild(tdArtist);
+            tr.appendChild(tdTitle);
+            tr.appendChild(tdPrice);
+            tr.appendChild(tdYear);
+
+            // append row to Tbody
+            tbody.appendChild(tr);
     }
-    document.getElementById("tableBody").innerHTML = content;
-    document.getElementById("demo").style.display = "table";
+    // append Tbody to the demo table
+    table.appendChild(tbody);
+
+    // show the table
+    table.style.display = "table";
 }
