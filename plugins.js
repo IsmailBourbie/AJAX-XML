20ea6e5572d1597916d386c34a53eb61ef9d7396
IsmailBourbie <bourbieismail@gmail.com>
2020-01-17 23:47:30 +0100

    add search function, finish the project

--- plugins.js
+++ plugins.js
@@ -128,3 +128,27 @@ function sortTable(tdIndex) {
         }
     }
 }
+
+// Search in table
+document.getElementById('searchInput').addEventListener('keyup', search);
+function search() {
+    // Declare variables
+    var input, filter, tableBody, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    tableBody = document.getElementById("tableBody");
+    tr = tableBody.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
